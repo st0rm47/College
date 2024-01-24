00c4d006bed1bd5f478a24f06ae70ea339e3444b
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2024-01-24 20:06:35 +0545

    DSA LAB 2

--- DSA/Lab2.cpp
+++ DSA/Lab2.cpp
@@ -10,12 +10,48 @@ int item, ch;
 void push(int stack[], int item)
 {
     if (top==(max_size-1))
-    {
         cout << "Stack is Full" << endl;
-    }
     else
     {
         top+=1;
         stack[top]=item;
+    }
+}
+
+//Pop from Stack
+void pop(int stack[])
+{
+    int top_item;
+    if(top==-1)
+        cout << "Stack is Empty" << endl;
+    else
+    {
+        top_item = stack[top];
+        top-=1;
+        cout << "Popped item is " << top_item << endl;
+    }
+}
+
+//Display from Stack
+void display(int stack[])
+{
+    if(top==-1)
+        cout << "Stack is Empty" << endl;
+    else
+    {
+        for (int i = top; i>=0; i--)
+            cout << stack[i] << endl;
+    }
+}
+
+int main()
+{
+    while(1)
+    {
+        cout << "Program Menu" << endl;
+        cout << "1. Push an Element\n"
+             << "2. Pop an Element\n"
+             << "3. Display Stack\n"
+             << "4. Exit \n";
     }
 }
