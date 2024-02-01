8bc7275f4429a9f76677e37277854df3bf71d1ec
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2024-02-01 14:30:17 +0545

    Lab 7 for DSA Fibonacci Sequence

---
+++ DSA/Lab7.cpp
@@ -0,0 +1,23 @@
+// Write a recursive program to display the series of n number of fibonacci sequences.
+
+#include<iostream>
+using namespace std;
+
+int fibo(int n){
+    if (n==0)
+        return 0;
+    else if(n==1)
+        return 1;
+    else
+        return(fibo(n-1)+fibo(n-2));
+}
+int main()
+{
+    int n,i;
+    cout << "Enter a number:";
+    cin >> n;
+    cout << "Fibonacci series:" << endl;
+    for(i=0;i<n;i++)
+        cout << fibo(i) << "\t" ;
+    return 0;
+}
