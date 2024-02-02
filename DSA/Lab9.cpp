bf95c42a63e904108a43ead29605658a2094cc7d
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2024-02-02 07:40:50 +0545

    Tower of Hanoi

--- DSA/Lab9.cpp
+++ DSA/Lab9.cpp
@@ -5,18 +5,18 @@ using namespace std;
 int count = 0;
 
 // Function to solve the Tower of Hanoi problem
-void towerOfHanoi(int n, char source, char temp, char destination) 
+void towerOfHanoi(int n, char source, char temp, char destination)
 {
     if (n == 1)
     {
-        //Move the top disk from source to destination
+        // Move the top disk from source to destination
         cout << "Move disk 1 from " << source << " to " << destination << endl;
         count++;
         return;
     }
     // Move (n-1) disks from source to temporary peg using destination as the temporary peg
     towerOfHanoi(n - 1, source, destination, temp);
-    count ++;
+    count++;
 
     // Move the nth disk from source to destination peg
     cout << "Move disk " << n << " from " << source << " to " << destination << endl;
@@ -24,8 +24,7 @@ void towerOfHanoi(int n, char source, char temp, char destination)
     // Move (n-1) disks from temporary peg to destination peg using source as the temporary peg
     towerOfHanoi(n - 1, temp, source, destination);
 }
-
-int main() 
+int main()
 {
     cout << "\t\t====================" << endl;
     cout << "\t\t   Tower of Hanoi   " << endl;
@@ -39,7 +38,7 @@ int main()
         cout << "Invalid input. Please enter a positive integer." << endl;
     else
         cout << "Moves to solve the Tower of Hanoi problem with " << numDisks << " disks:" << endl;
-        towerOfHanoi(numDisks, 'A', 'B', 'C');
+    towerOfHanoi(numDisks, 'A', 'B', 'C');
 
     cout << "Total Number of Steps: " << count << endl;
     return 0;
