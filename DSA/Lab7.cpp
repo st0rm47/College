46ce43992093408e84b0dca387296aa658c51b0b
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2024-02-03 17:27:48 +0545

    Fibonacci Sequences

--- DSA/Lab7.cpp
+++ DSA/Lab7.cpp
@@ -3,24 +3,24 @@
 #include <iostream>
 using namespace std;
 
-// Function to calculate fibonacci series using recursion
-int fibo(int n)
+// Function to calculate the nth term of the Fibonacci sequence using recursion
+int fibo(int n) 
 {
     if (n <= 1)
         return n;
     else
-        return (fibo(n - 1) + fibo(n - 2));
+        return fibo(n - 1) + fibo(n - 2);
 }
-int main()
+
+int main() 
 {
     cout << "\t\t========================" << endl;
     cout << "\t\t  Fibonacci Calculator  " << endl;
     cout << "\t\t========================" << endl;
-
-    int n, i;
+    
+    int i,n;
     cout << "Enter a non-negative integer: ";
     cin >> n;
-
     cout << "Fibonacci series:" << endl;
     for (i = 0; i < n; i++)
         cout << fibo(i) << "\t";
