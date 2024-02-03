9334bd15b6f7fe2a0b2ea1409f4305c664aeac14
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2024-02-03 17:15:44 +0545

    Singly Linked List Implementation

--- DSA/Lab5.cpp
+++ DSA/Lab5.cpp
@@ -1,64 +1,80 @@
-// wap to insert a node at the end of singly linked list
-#include<iostream>
+// Wap to show the implementation of singly linked list
+
+#include <iostream>
 using namespace std;
 
-struct node {
-    int data;
-    node* next;
+// Node structure for the singly linked list
+struct node
+{
+    int data;      
+    node *next;    
 };
 
-void insertAtBEG(node*& head, int value) {
-    node* new_node = (node*) malloc (sizeof(node));
+// Function to insert a new node at the beginning of the linked list
+void insertAtBEG(node *&head, int value)
+{
+    node *new_node = (node *)malloc(sizeof(node));
     new_node->data = value;
     new_node->next = nullptr;
-    
-    if(head == nullptr){
+    if (head == nullptr)
         head = new_node;
-        new_node->next = nullptr;
-    }
-    else{
+    else
+    {
         new_node->next = head;
         head = new_node;
     }
-
 }
 
-void insertAtEND(node*& head, int value) {
-    node* new_node = (node*) malloc (sizeof(node));
+// Function to insert a new node at the end of the linked list
+void insertAtEND(node *&head, int value)
+{
+    node *new_node = (node *)malloc(sizeof(node));
     new_node->data = value;
     new_node->next = nullptr;
-    
-    if (head == nullptr) {
+    if (head == nullptr)
+    {
         cout << "List is Empty";
         head = new_node;
-    } else {
-        node* temp = head;
-        while (temp->next != nullptr) {
+    }
+    else
+    {
+        node *temp = head;
+        while (temp->next != nullptr)
+        {
             temp = temp->next;
         }
         temp->next = new_node;
     }
 }
 
-void deleteFromBEG(node*& head) {
-    if (head == nullptr) {
+// Function to delete a node from the beginning of the linked list
+void deleteFromBEG(node *&head)
+{
+    if (head == nullptr)
         cout << "List is Empty. Cannot delete from the beginning." << endl;
-    } else {
-        node* temp = head;
+    else
+    {
+        node *temp = head;
         head = head->next;
         free(temp);
     }
 }
 
-void deleteFromEND(node*& head) {
-    if (head == nullptr) {
+// Function to delete a node from the end of the linked list
+void deleteFromEND(node *&head)
+{
+    if (head == nullptr)
         cout << "List is Empty" << endl;
-    } else if (head->next == nullptr) {
+    else if (head->next == nullptr)
+    {
         free(head);
         head = nullptr;
-    } else {
-        node* temp = head;
-        while (temp->next->next != nullptr) {
+    }
+    else
+    {
+        node *temp = head;
+        while (temp->next->next != nullptr)
+        {
             temp = temp->next;
         }
         free(temp->next);
@@ -66,36 +82,44 @@ void deleteFromEND(node*& head) {
     }
 }
 
-void displayList(node* head) {
-    if (head == nullptr) {
+// Function to display the linked list
+void displayList(node *head)
+{
+    if (head == nullptr)
         cout << "List is Empty" << endl;
-    }
-
-    node* temp = head;
-    while(temp != nullptr) {
+    
+    // Traverse and display each node's data
+    node *temp = head;
+    while (temp != nullptr)
+    {
         cout << temp->data << " ";
         temp = temp->next;
     }
     cout << endl;
 }
 
-int main() {
-    node* head = nullptr;
-    int ch, item;
+int main()
+{  
+    cout << "\t\t=====================================" << endl;
+    cout << "\t\t  Singly Linked List Implementation  " << endl;
+    cout << "\t\t=====================================" << endl;
 
-    while (true) {
-        cout << "Program Menu\n";
-        cout << "1. Insert at the Beginning.\n";
-        cout << "2. Insert at the End.\n";
-        cout << "3. Delete from the Beginning.\n";
-        cout << "4. Delete from the End.\n";
-        cout << "5. Display the List.\n";
-        cout << "6. EXIT.\n";
-        
+    node *head = nullptr;
+    int ch, item;
+    while (1)
+    {
+        cout << endl;
+        cout << "Program Menu" << endl;
+        cout << "1. Insert at the Beginning." << endl;
+        cout << "2. Insert at the End." << endl;
+        cout << "3. Delete from the Beginning." << endl;
+        cout << "4. Delete from the End." << endl;
+        cout << "5. Display the List." << endl;
+        cout << "6. EXIT." << endl;
         cout << "Enter your Choice:";
         cin >> ch;
-
-        switch (ch) {
+        switch (ch)
+        {
             case 1:
                 cout << "Enter an element:";
                 cin >> item;
@@ -118,10 +142,9 @@ int main() {
             case 6:
                 exit(0);
             default:
-                cout << "Invalid choice! Please enter a valid option." << endl;
+                cout << "Invalid choice!" << endl;
                 break;
         }
     }
-
     return 0;
 }
