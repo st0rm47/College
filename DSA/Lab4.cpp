2666af1645ac37150f7a4f69a018586049101d47
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2024-02-03 16:05:58 +0545

    Circular Queue Implementation

--- DSA/Lab4.cpp
+++ DSA/Lab4.cpp
@@ -1,5 +1,7 @@
+// WAP to implement the operations of circular queue.
+
 #include <iostream>
-#define max_size 5
+#define max_size 3
 using namespace std;
 
 int circular_queue[max_size];
@@ -8,15 +10,11 @@ int front = -1, rear = -1;
 void enqueue(int x)
 {
     if (front == (rear + 1) % max_size)
-    {
         cout << "Queue is Full" << endl;
-    }
     else
     {
         if (front == -1)
-        {
             front = 0;
-        }
         rear = (rear + 1) % max_size;
         circular_queue[rear] = x;
     }
@@ -25,59 +23,59 @@ void enqueue(int x)
 void dequeue()
 {
     if (front == -1)
-    {
         cout << "Queue is empty" << endl;
-    }
     else
-    {
         front = (front + 1) % max_size;
-    }
 }
 
 void display()
 {
     if (front == -1)
-    {
         cout << "Queue is empty" << endl;
-        return;
-    }
-    for (int i = front; i <= rear; i++)
+    int i = front;
+    do
     {
         cout << circular_queue[i] << "  ";
-    }
+        i = (i + 1) % max_size;
+    } while (i != (rear + 1) % max_size);
     cout << endl;
 }
 
 int main()
 {
+    cout << "\t\t=================================" << endl;
+    cout << "\t\t  Circular Queue Implementation  " << endl;
+    cout << "\t\t=================================" << endl;
+
     int ch, item;
-    while (true)
+    while (1)
     {
-        cout << "Program Menu\n";
-        cout << "1. Enqueue an Element.\n";
-        cout << "2. Dequeue an Element.\n";
-        cout << "3. Display Queue.\n";
-        cout << "4. Exit\n";
+        cout << endl;
+        cout << "Program Menu" << endl;
+        cout << "1. Enqueue an Element." << endl;
+        cout << "2. Dequeue an Element." << endl;
+        cout << "3. Display Queue." << endl;
+        cout << "4. Exit." << endl;
         cout << "Enter your Choice:";
         cin >> ch;
         switch (ch)
         {
-        case 1:
-            cout << "Enter an element:";
-            cin >> item;
-            enqueue(item);
-            break;
-        case 2:
-            dequeue();
-            break;
-        case 3:
-            display();
-            break;
-        case 4:
-            exit(0);
-        default:
-            cout << "Invalid choice! Please enter a valid option." << endl;
-            break;
+            case 1:
+                cout << "Enter an element:";
+                cin >> item;
+                enqueue(item);
+                break;
+            case 2:
+                dequeue();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                exit(0);
+            default:
+                cout << "Invalid choice!" << endl;
+                break;
         }
     }
     return 0;
