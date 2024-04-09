cd3034847a2ff906c382c9c6494f66f39c21f643
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2024-04-09 19:05:45 +0545

    BLA Algorithm (Negative Slope)

---
+++ CG/BLA_Negative_Slope.cpp
@@ -0,0 +1,40 @@
+// 4. BLA algorithm for negative slope
+
+#include <iostream>
+#include <graphics.h>
+using namespace std;
+
+void BLA(int X1, int Y1, int X2, int Y2){
+    int x = X1, y = Y1;
+    int dx = X2 - X1;
+    int dy = Y2 - Y1;
+    putpixel(x,y,WHITE);
+    int p = (2 * dy - dx);
+    
+    while (x >= X2){
+        if (p < 0){
+            x--;
+            p = p + (2 * dy) + (2 * dx); 
+        }
+        else{
+            x--;
+            y++;
+            p = p + (2 * dy)- (2 * dx);
+        }
+        putpixel(x,y,WHITE);
+    }
+}
+int main(){
+    int x1, y1, x2, y2;
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, "");
+
+    cout << "Enter the coordinates of the first point: ";
+    cin >> x1 >> y1;
+    cout << "Enter the coordinates of the second point: ";
+    cin >> x2 >> y2;
+
+    BLA(x1, y1, x2, y2);
+    getch();
+    closegraph();
+}
