5d8fdf75cfe8447a62fad8f2057b50e7eae66f53
Subodh Ghimire <ghimiresubodh59@gmail.com>
2024-04-21 17:32:55 +0545

    Newton Divided Difference Interpolation

--- NM/divided_difference.c
+++ NM/divided_difference.c
@@ -1,47 +1,34 @@
-//divided difference interpolation
 #include <stdio.h>
 #include <math.h>
-#include <conio.h>
 int main()
 {
-	float x,v,p;
-	int i,j,n;
-	printf("Enter the number of points: ");
-	scanf("%d",&n);
-	printf("Enter value of x: ");
-	scanf("%f",&x);
-	float xi[n],dd[n],fx[n];
-	for (i=0;i<n;i++)
-	{
-		printf("\nPoint %d:\n",i+1);
-		printf("Enter x: ");
-		scanf("%f",&xi[i]);
-		printf("Enter fx: ");
-		scanf("%f",&fx[i]);
+    float x[15], fx[15], dd[15], xp, v, p;
+    int i, j, n;
+    printf("Divided Difference Interpolation\n\n");
+    printf("Enter the number of data pairs: ");
+    scanf("%d", &n);
+    printf("Enter data pairs x(i) and values f(i): ");
+    for (i = 0; i < n; i++)
+        scanf("%f%f", &x[i], &fx[i]);
+    printf("\nEnter the value at which interpolation is required: ");
+    scanf("%f", &xp);
+    for (i = 0; i < n; i++){
+        dd[i] = fx[i];
+	} 
+	for (i = 0; i < n; i++){
+        for (j = n - 1; j > i; j--){
+            dd[j] = (dd[j] - dd[j - 1]) / (x[j] - x[j - 1 - i]);
+    	}
 	}
-	for(i=0;i<n;i++)
-	{
-		dd[i]=fx[i];
-	}
-
-	for(i=0;i<n;i++)
-	{
-		for(j=n-1;j>i;j--)
-		{
-			dd[j]=(dd[j]-dd[j-1])/(xi[j]-xi[j-1-i]);
-		}
-	}
-	v=0;
-	p=1;
-	for(i=0;i<n;i++)
-	{
-		for(j=0;j<i;j++)
-		{
-			p=p*(x-xi[j]);
-		}
-		v=v+dd[i]*p;
-		p=1;
-	}
-	printf("v = %f",v);
-	return 0;
+    v = 0;
+    p = 1;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < i; j++)
+            p = p * (xp - x[j]);
+        v = v + dd[i] * p;
+        p = 1;
+    }
+    printf("Interpolated function value at x = %.4f is %.4f.", xp, v);
+    return 0;
 }
