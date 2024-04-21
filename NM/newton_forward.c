f9be5b32940457f6c421ffe90ea7af0429b07d96
Subodh Ghimire <ghimiresubodh59@gmail.com>
2024-04-21 18:26:06 +0545

    Newton Forward Interpolation

--- NM/newton_forward.c
+++ NM/newton_forward.c
@@ -7,33 +7,33 @@ int fac(int n){
 }
 int main() {
     int n, i, j, k;
-    float a[n], b[n], x[n], xp, h, s;
+    float x[n], fx[n], dd[n], xp, h, s;
     printf("Forward Difference Interpolation\n\n");
     printf("Enter the number of data pairs: ");
     scanf("%d", &n);
     printf("Enter data pairs x(i) and values f(i): ");
     for (i = 0; i < n; i++)
-        scanf("%f%f", &a[i], &b[i]);
-    printf("Enter the value of a where you want to evaluate b: ");
+        scanf("%f%f", &x[i], &fx[i]);
+    printf("\nEnter the value at which interpolation is required: ");
     scanf("%f", &xp);
-    h = a[1] - a[0];
-    s = (xp - a[0]) / h;
+    h = x[1] - x[0];
+    s = (xp - x[0]) / h;
     for (i = 0; i < n; i++) {
-        x[i] = b[i];
+        dd[i] = fx[i];
     }
     for (i = 0; i <= n - 1; i++) {
         for (j = n - 1; j >= i + 1; j--) {
-            x[j] = x[j] - x[j - 1];
+            dd[j] = dd[j] - dd[j - 1];
         }
     }
     float v, p;
-    v = x[0];
+    v = dd[0];
     for (i = 1; i <= n - 1; i++) {
         p = 1;
         for (k = 1; k <= i; k++) {
             p = p * (s - k + 1);
         }
-        v = v + ((x[i] * p) / fac(i));
+        v = v + ((dd[i] * p) / fac(i));
     }
     printf("The interpolated value of a=%.4f is %.4f", xp, v);
     return 0;
