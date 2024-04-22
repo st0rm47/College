48a2abe7978ae56f744d6f57da62b0b624652a96
Subodh Ghimire <ghimiresubodh59@gmail.com>
2024-04-22 17:15:13 +0545

    Backward Difference Formula

---
+++ NM/Backward_difference.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int a3, a2, a1, a0;
+double f(double x){
+    return (a3 * x * x * x + a2 * x * x + a1 * x + a0);
+}
+int main(){
+    double h, x;
+    printf("Backward Difference Formula\n\n");
+    printf("Enter values of a3, a2, a1, and a0 respectively: ");
+    scanf("%d%d%d%d", &a3, &a2, &a1, &a0);
+    printf("Enter value of h: ");
+    scanf("%lf", &h);
+    printf("Enter value at which derivative is needed: ");
+    scanf("%lf", &x);
+    double d = (f(x) - f(x - h)) / h;
+    printf("Derivative = %.2lf", d);
+    return 0;
+}
