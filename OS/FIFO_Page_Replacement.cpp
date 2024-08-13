912600d2939c4128100d44a8be750a3576938785
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2024-08-13 20:25:26 +0545

    FIFO

--- OS/FIFO_Page_Replacement.cpp
+++ OS/FIFO_Page_Replacement.cpp
@@ -12,9 +12,9 @@ int fifoPageReplacement(const vector<int> &page_references, int frame_size)
     int page_faults = 0;
 
     // Print the table header
-    cout << "+-----------------+----------+----------+----------+---------------+" << endl;
-    cout << "| Page Reference  | Frame 1  | Frame 2  | Frame 3  | Page Fault    |" << endl;
-    cout << "+-----------------+----------+----------+----------+---------------+" << endl;
+    cout << "+----------------+---------+---------+---------+------------+" << endl;
+    cout << "| Page Reference | Frame 1 | Frame 2 | Frame 3 | Page Fault |" << endl;
+    cout << "+----------------+---------+---------+---------+------------+" << endl;
 
     for (int page : page_references)
     {
@@ -42,16 +42,16 @@ int fifoPageReplacement(const vector<int> &page_references, int frame_size)
         }
 
         // Display the current frame status
-        cout << "| " << setw(15) << page << " |";
+        cout << "| " << setw(10) << page << "     |";
         for (int i = 0; i < frame_size; i++)
         {
             if (i < frames.size())
-                cout << setw(8) << frame_content[i] << "  |";
+                cout << setw(5) << frame_content[i] << "    |";
             else
-                cout << setw(8) << " " << "  |";
+                cout << setw(5) << " " << "    |";
         }
-        cout << setw(11) << (page_fault_occurred ? "Yes" : "No") << "    |" << endl;
-        cout << "+-----------------+----------+----------+----------+---------------+" << endl;
+        cout << setw(10) << (page_fault_occurred ? "Yes" : "No") << "  |" << endl;
+        cout << "+----------------+---------+---------+---------+------------+" << endl;
     }
     return page_faults;
 }
@@ -75,5 +75,6 @@ int main()
 
     int page_faults = fifoPageReplacement(page_references, frame_size);
     cout << endl << "Total number of page faults: " << page_faults << endl;
+    cout << endl << "Compiled by : Subodh Ghimire " << endl;
     return 0;
 }
