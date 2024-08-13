adcc16a7cd0ea96278d73340e29f8b440499a980
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2024-08-13 21:02:55 +0545

    LRU

--- OS/LRU_Page_Replacement.cpp
+++ OS/LRU_Page_Replacement.cpp
@@ -1,70 +1,70 @@
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <deque>
 #include <algorithm>
 #include <iomanip>
+
 using namespace std;
 
 int lruPageReplacement(const vector<int> &page_references, int frame_size) {
-    queue<int> frames;  // Holds the current frames (order of use)
-    vector<int> frame_content(frame_size, -1);  // Stores the page numbers in frames
+    deque<int> frames;    // Use deque to manage LRU efficiently
+    vector<int> frame_content(frame_size, -1); // To keep track of frame contents
     int page_faults = 0;
 
     // Print the table header
-    cout << "+-----------------+----------+----------+----------+---------------+" << endl;
-    cout << "| Page Reference  | Frame 1  | Frame 2  | Frame 3  | Page Fault    |" << endl;
-    cout << "+-----------------+----------+----------+----------+---------------+" << endl;
+    cout << "+----------------+---------+---------+---------+------------+" << endl;
+    cout << "| Page Reference | Frame 1 | Frame 2 | Frame 3 | Page Fault |" << endl;
+    cout << "+----------------+---------+---------+---------+------------+" << endl;
 
     for (int page : page_references) {
         bool page_fault_occurred = false;
-        bool page_in_frames = false;
+        auto it = find(frames.begin(), frames.end(), page);
 
-        // Check if the page is already in frames
-        for (int i = 0; i < frame_size; i++) {
-            if (frame_content[i] == page) {
-                page_in_frames = true;
-                break;
-            }
-        }
-
-        if (!page_in_frames) {
+        if (it == frames.end()) {
             // Page fault occurs
-            if (frames.size() < frame_size) {
-                frames.push(page);
-                frame_content[frames.size() - 1] = page;
-            } else {
+            if (frames.size() >= frame_size) {
                 // Remove the least recently used page
                 int lru_page = frames.front();
-                frames.pop();
-                // Replace it with the new page
-                frames.push(page);
+                frames.pop_front();
                 // Update the frame content
-                auto it = find(frame_content.begin(), frame_content.end(), lru_page);
-                if (it != frame_content.end()) {
-                    *it = page;
+                auto frame_it = find(frame_content.begin(), frame_content.end(), lru_page);
+                if (frame_it != frame_content.end()) {
+                    *frame_it = -1; // Mark as empty
                 }
+            }
+            frames.push_back(page);
+            // Update the frame content
+            auto empty_it = find(frame_content.begin(), frame_content.end(), -1);
+            if (empty_it != frame_content.end()) {
+                *empty_it = page;
             }
             page_faults++;
             page_fault_occurred = true;
+        } else {
+            // Page is already in the frame, move it to the back
+            frames.erase(it);
+            frames.push_back(page);
         }
 
         // Display the current frame status
-        cout << "| " << setw(15) << page << " |";
-        for (int i = 0; i < frame_size; i++) {
+        cout << "| " << setw(10) << page << "     |";
+        for (int i = 0; i < frame_size; i++)
+        {
             if (i < frames.size())
-                cout << setw(8) << frame_content[i] << "  |";
+                cout << setw(5) << frame_content[i] << "    |";
             else
-                cout << setw(8) << " " << "  |";
+                cout << setw(5) << " " << "    |";
         }
-        cout << setw(11) << (page_fault_occurred ? "Yes" : "No") << "    |" << endl;
-        cout << "+-----------------+----------+----------+----------+---------------+" << endl;
+        cout << setw(10) << (page_fault_occurred ? "Yes" : "No") << "  |" << endl;
+        cout << "+----------------+---------+---------+---------+------------+" << endl;
     }
+
     return page_faults;
 }
 
 int main() {
     cout << "\t\t=========================" << endl;
-    cout << "\t\t    LRU Page Replacement  " << endl;
+    cout << "\t\t  LRU Page Replacement  " << endl;
     cout << "\t\t=========================" << endl;
 
     int n, frame_size;
@@ -80,5 +80,6 @@ int main() {
 
     int page_faults = lruPageReplacement(page_references, frame_size);
     cout << endl << "Total number of page faults: " << page_faults << endl;
+    cout << endl << "Compiled by : Subodh Ghimire " << endl;
     return 0;
 }
