14e1a64631afef603efb073805082bb76a270aa5
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2025-03-06 20:16:12 +0545

    JavaScript

---
+++ Web_Technology/JavaScript/day_2.js
@@ -0,0 +1,10 @@
+function hello1(){
+    document.getElementById("title").innerHTML = "This is JavaScript Language";
+    document.body.style.color= "lightblue";
+}
+
+function hello2(){
+    document.getElementById("title").innerHTML = "JavaScript";
+    document.body.style.color= "lightblue";
+}
+
---
+++ Web_Technology/JavaScript/day_3.js
@@ -0,0 +1,15 @@
+function change(color){
+    document.body.bgColor = color;
+
+}
+
+function store(){
+    var a = prompt('Enter a color');
+    change(a);
+}
+
+function img(image){
+    document.body.background = image;
+}
+
+
