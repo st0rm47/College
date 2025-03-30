d442d4311cf0bf0d6bdb517af675f60bf58c65a9
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2025-03-30 21:13:30 +0545

    Heap Sort

--- DAA/Heap_Sort.cpp
+++ DAA/Heap_Sort.cpp
@@ -1,34 +1,63 @@
 #include<iostream>
 using namespace std;
 
-void heapify(int arr[], int n, int i) {
+void printArray(int arr[], int n) 
+{
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+// Function to heapify a subtree rooted at index i
+void heapify(int arr[], int n, int i) 
+{
     int largest = i;
     int left = 2*i + 1;
     int right = 2*i + 2;
 
-    if(left < n && arr[left] > arr[largest]) {
+    cout << endl << "[Heapify] Called on index " << i << " : ";
+    printArray(arr, n);
+
+    if(left < n && arr[left] > arr[largest])
         largest = left;
-    }
 
-    if(right < n && arr[right] > arr[largest]) {
+    if(right < n && arr[right] > arr[largest])
         largest = right;
-    }
 
-    if(largest != i) {
+    if(largest != i) 
+    {
+        cout << " - Swapping " << arr[i] << " and " << arr[largest] << endl;
         swap(arr[i], arr[largest]);
-        heapify(arr, n, largest);
+        cout << " - After swap: ";
+        printArray(arr, n);
+        heapify(arr, n, largest); // Recursive heapify
     }
 }
 
-void heapSort(int arr[], int n) {
-    for(int i = n/2 - 1; i >= 0; i--) {
+// Function to perform heap sort
+void heapSort(int arr[], int n) 
+{
+    cout << "=============================" << endl;
+    cout << "  Step 1: Building Max Heap  " << endl;
+    cout << "=============================" << endl;
+    for(int i = n/2 - 1; i >= 0; i--) 
+    {
         heapify(arr, n, i);
     }
-
-    for(int i = n-1; i >= 0; i--) {
+    cout << "------------------------------------" << endl;
+    cout << endl;
+    cout << "===================" << endl;
+    cout << "  Step 2: Sorting  " << endl;
+    cout << "===================" << endl;
+    for(int i = n - 1; i > 0; i--) 
+    {
+        cout << "[Swap Max] Swapping " << arr[0] << " and " << arr[i] << endl;
         swap(arr[0], arr[i]);
+        cout << " - After swap: ";
+        printArray(arr, n);
         heapify(arr, i, 0);
     }
+    cout << "------------------------------------" << endl;
 }
 
 int main()
@@ -44,20 +73,17 @@ int main()
     cout << "Enter the elements: ";
     for (int i = 0; i < n; i++)
         cin >> A[i];
+    cout << endl;
 
-    // Display original array
     cout << "Original array: ";
-    for (int i = 0; i < n; i++)
-        cout << A[i] << " ";
+    printArray(A, n);
     cout << endl;
 
     // Perform heap sort
     heapSort(A, n);
 
-    // Display sorted array
-    cout << "Sorted array: ";
-    for (int i = 0; i < n; i++)
-        cout << A[i] << " ";
+    cout << endl << "Final Sorted Array: ";
+    printArray(A, n);
 
     cout << "\nCompiled by : Subodh Ghimire " << endl;
     return 0;
