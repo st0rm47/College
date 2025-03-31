45e2eebc8803152c72d0cd30c6b02c4c12aee42c
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2025-03-31 21:13:16 +0545

    Linear Search

---
+++ DAA/Linear_Search.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+// Function to perform linear search
+void linearSearch(int A[], int n, int key)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (A[i] == key)
+        {
+            cout << "Element found at position " << i << endl;
+            return;
+        }
+    }
+    cout << "Element not found" << endl;
+}
+
+int main()
+{
+    cout << "\t\t=================" << endl;
+    cout << "\t\t  Linear Search  " << endl;
+    cout << "\t\t=================" << endl;
+    
+    int n;
+    int A[100];
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> A[i];
+
+    int key;
+    cout << "Enter the element to search: ";
+    cin >> key;
+
+    // Perform linear search
+    linearSearch(A, n, key);
+
+    cout << "\nCompiled by : Subodh Ghimire " << endl;
+    return 0;
+}
