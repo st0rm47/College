5565fcd152b5847adf00d8f4fb9880b4e7e1662a
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2025-07-10 18:49:27 +0545

    Lab 2

---
+++ Dot_NET/Lab2.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("First C# Program for NET Centric Computing \n");
+            Console.WriteLine("Made by: Subodh Ghimire");
+            Console.ReadLine();
+        }
+    }
+}
