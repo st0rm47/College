d5e3ab1fb001d01d2dfab342c8f3dc14d0f39210
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2025-07-10 18:50:09 +0545

    Lab 5

---
+++ Dot_NET/Lab5.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5 {
+    internal class Program {
+        static void Main(string[] args) {
+            int x = 5;
+            int y = 6;
+            bool c = x > y;
+            Console.WriteLine(c);
+            Console.WriteLine("Made by: Subodh Ghimire");
+            Console.ReadLine();
+        }
+    }
+}
