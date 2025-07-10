507d86e625ee9029ae7e663faa32c2e61b73074c
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2025-07-10 18:50:17 +0545

    Lab 6

---
+++ Dot_NET/Lab6.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6 {
+    internal class Program {
+        static void Main(string[] args) {
+            float a = 23.45678901f;
+            double b = 1234567890.0987654321d;
+            decimal c = 123.456m;
+            Console.WriteLine("Float: " + a);
+            Console.WriteLine("Double: " + b);
+            Console.WriteLine("Decimal: " + c);
+            Console.WriteLine("Made by: Subodh Ghimire");
+            Console.ReadLine();
+        }
+    }
+}
