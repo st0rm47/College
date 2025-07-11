781e3479f57b866b3d421555d0316cc9636276ab
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2025-07-11 18:50:02 +0545

    Lab 8

---
+++ Dot_NET/Lab8.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8 {
+    internal class Program {
+        static void Main(string[] args) {
+            string x = @"D:\Subodh\Tutorials\CSharp";
+            Console.WriteLine(x);
+            string y = "D:\\Subodh\\Tutorials\\CSharp";
+            Console.WriteLine(y);
+            Console.WriteLine("Made by: Subodh Ghimire");
+            Console.ReadLine();
+        }
+    }
+}
