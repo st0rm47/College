bf553ff94f792b044868e0f3ed80283f3eb3ed55
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2025-07-12 18:50:36 +0545

    Lab 9

---
+++ Dot_NET/Lab9/Lab9a.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Lab9a {
+    internal class Program {
+        static void Main(string[] args) {
+            int a = 20;
+            float b = a;
+            Console.WriteLine("Implicit Conversion (int to float): " + b + "\n");
+            Console.WriteLine("Made by: Subodh Ghimire");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Dot_NET/Lab9/Lab9b.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Lab9b {
+    internal class Program {
+        static void Main(string[] args) {
+            float x = 10.5f;
+            int y = Convert.ToInt32(x);
+            Console.WriteLine("Explicit Conversion (float to int): " + y + "\n");
+            Console.WriteLine("Made by: Subodh Ghimire");
+            Console.ReadLine();
+        }
+    }
+}
