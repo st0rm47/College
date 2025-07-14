8f2b1cb2cb6141f2eb68ed24cfcbb269981f6f68
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2025-07-14 18:52:06 +0545

    Lab 13

---
+++ Dot_NET/Lab13/Lab13a.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Lab13a {
+    internal class Program {
+        static void Main(string[] args) {
+            int a = 40;
+            int b = 30;
+            bool c = (a > b) && (b < a);
+
+            Console.WriteLine($"a = {a}, b = {b}\n");
+            Console.WriteLine($"(a > b) && (b < a): {c}");
+            Console.WriteLine("\n Made by: Subodh Ghimire");
+            Console.ReadLine();
+        }
+    }
+}
