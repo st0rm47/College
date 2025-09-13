1f7bddd59a2e22f6161316037cb5140b597b1d80
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2025-09-13 14:49:13 +0545

    Lab 20

--- Dot_NET/Lab20.cs
+++ Dot_NET/Lab20.cs
@@ -4,44 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab20
-{
+namespace Lab20 {
     public delegate void Calculations(int a, int b);
-    internal class Program
-    {
-        public static void Addition(int a, int b)
-        {
+    internal class Program {
+        public static void Addition(int a, int b) {
             int result = a + b;
             Console.WriteLine("Addition: " + result);
         }
 
-        public static void Subtraction(int a, int b)
-        {
+        public static void Subtraction(int a, int b) {
             int result = a - b;
             Console.WriteLine("Subtraction: " + result);
         }
 
-        public static void Multiplication(int a, int b)
-        {
+        public static void Multiplication(int a, int b) {
             int result = a * b;
             Console.WriteLine("Multiplication: " + result);
         }
 
-        public static void Division(int a, int b)
-        {
-            if (b != 0)
-            {
+        public static void Division(int a, int b) {
+            if (b != 0) {
                 double result = (double)a / b;
                 Console.WriteLine("Division: " + result);
-            }
-            else
-            {
+            } else {
                 Console.WriteLine("Division by zero is not allowed.");
             }
         }
 
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
+
+            Console.WriteLine($"Initial values:\na = {10}, b = {5}\n");
             Calculations  calc = new Calculations(Program.Addition);
             calc.Invoke(10, 5);
 
