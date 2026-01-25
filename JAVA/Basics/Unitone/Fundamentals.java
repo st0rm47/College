e06a3981c2a25d252ddeaa683aa2da3e80da9c14
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2026-01-25 10:19:11 +0545

    feat: add basic Java fundamentals examples

---
+++ JAVA/Basics/Unitone/Fundamentals.java
@@ -0,0 +1,23 @@
+package Unitone;
+
+class Fundamentals {
+    public static void main(String[] args) {
+        // This is a comment
+
+        /*
+        This is a
+        multiline
+        comment
+         */
+
+        // Text Output
+        System.out.println("Hello World");
+
+        // Number Output
+        System.out.println(5 + 5);
+
+        // Print Method
+        System.out.print("Hello World ");
+        System.out.print("It will print on same line");
+    }
+}
