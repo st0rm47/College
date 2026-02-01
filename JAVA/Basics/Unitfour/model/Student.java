3b629e73fc3efddd88fb9f27db91312349c4b4da
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2026-02-01 10:44:58 +0545

    feat: create Student model for database CRUD operations

---
+++ JAVA/Basics/Unitfour/model/Student.java
@@ -0,0 +1,26 @@
+package Unitfour.model;
+
+public class Student {
+
+    /// Attributes
+    private int id;
+    private String name;
+
+    /// Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
